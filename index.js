51ec4207c2c29c15d6ce76f4e08d6019f8d1985e
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-12 11:42:32 -0500

    commit tree aa31a0924b1d0a5eeea53b01e75df7cb269d03a2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+]
+// function superBowlWin(records) {
+//   const firstWin = records.find((result) => result === "W")
+//   return firstWin
+// }
+// superBowlWin(record)
+
+// const superbowlWin = (record) => {
+//   const result = record.find((record) => record.result === "W")
+//   return result ? result.year : undefined
+// }
+function superbowlWin(recordsArray) {
+  let winRecord = recordsArray.find((rec) => rec.result === "W")
+  if (winRecord) {
+    return winRecord.year
+  }
+}
